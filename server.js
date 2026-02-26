958a9038c4cc874ebca3076bd27fdaa769c3adcf
Shyi1 <95385235+Shyi1@users.noreply.github.com>
2026-02-26 01:42:32 +0000

    Update server.js

--- server.js
+++ server.js
@@ -22,18 +22,16 @@ app.use(express.json());
 
 // Firebase Connection
 
-if (!admin.apps.length) {
+const admin = require("firebase-admin");
 
-    admin.initializeApp({
-
-        credential: admin.credential.cert({
-
-            projectId: process.env.FIREBASE_PROJECT_ID,
-
-            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-
-            privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+  }),
+  databaseURL: "https://omllagame-default-rtdb.europe-west1.firebasedatabase.app/"
+});
         }),
 
         databaseURL: process.env.FIREBASE_DB_URL
@@ -93,3 +91,4 @@ app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server started on port ${PORT}`);
 
 });
+
